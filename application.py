b8604549b98a1231115cb83f78eaff06ceac4e9f
lupin-oomura <lupin.Oomura@gmail.com>
2022-09-05 03:51:47 +0900

    fst

--- application.py
+++ application.py
@@ -17,7 +17,8 @@ def upload_file():
     if request.method == 'POST':
         f = request.files["the_file"]
         #任意の階層をフルパスで指定(macの場合。任意のユーザー名は変更してください。)
-        f.save('files/' + secure_filename(f.filename))
+#        f.save('files/' + secure_filename(f.filename))
+        f.save(secure_filename(f.filename))
         #アップロードしてサーバーにファイルが保存されたらfinishedを表示
         return render_template('finished.html')
     else:
