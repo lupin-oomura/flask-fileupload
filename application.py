3ae65ef416f37248e999287f347337bf1b09514b
lupin-oomura <lupin.Oomura@gmail.com>
2022-09-05 03:43:42 +0900

    fst

--- application.py
+++ application.py
@@ -11,10 +11,10 @@ def hello():
 
 #from werkzeug.utils import secure_filename
 
-@app.route('/uploads', methods=['POST', 'GET'])
+@app.route('/uploads')
 def upload_file():
 
-    return render_template('uploads.html')
+    return render_template('render.html')
 
 
 
